56c0fda9549021c94f5561f881d96b6d9d7baf2d
thejohnmeyers <jmeyers5@gulls.salisbury.edu>
2023-09-27 22:40:38 -0400

    bruh

--- BattleShipModel.java
+++ BattleShipModel.java
@@ -2,31 +2,72 @@
 public class BattleShipModel {
 	private Board playerBoard;
 	private Board oppBoard;
-	Ship ship;
+	Ship[] ship = new Ship[5];
+	
 	public BattleShipModel(){
 		playerBoard = new Board();
-		oppBoard = new Board();
-		ship = new Ship(5);
 		
+		oppBoard = new Board();
+		ship[0] = new Ship(5);
+		ship[1] = new Ship(4);
+		ship[2] = new Ship(3);
+		ship[3] = new Ship(3);
+		ship[4] = new Ship(2);
+	}
+	public void callRandom() {
+		for(int i = 0; i < ship.length; i++) {
+			placeRandomPosition(ship[i]);
+		}
+		playerBoard.displayBoard();
+		System.out.println();
+		System.out.println();
+		System.out.println();
 	}
 	public void placeRandomPosition(Ship ship) {
 		int randomX = (int)(Math.random() * (10));
 		int randomY = (int)(Math.random() * (10));
-		int borderCheck = 1;
-		if((randomX+ship.size) > 10) {
-			borderCheck = -1;
-			for(int i = 0; i < ship.size; i++) {
-				playerBoard.gameBoard[randomX][randomY] = 1;
-				randomX+=borderCheck;
+		
+		int horoz = (int)(Math.random() * 2);
+		
+		if(horoz == 1) {
+			
+			if((ship.size + randomY) > 10) {
+				
+				for(int i = 0; i < ship.size; i++) {
+					playerBoard.gameBoard[randomX][randomY] = 1;
+					randomY-=1;
+				}
+				
 			}
+			else {
+				
+				for(int i = 0; i < ship.size; i++) {
+					playerBoard.gameBoard[randomX][randomY] = 1;
+					randomY+=1;
+				}
+				
+			}
+			
 		}
 		else {
-			for(int i = 0; i < ship.size; i++) {
-				playerBoard.gameBoard[randomX][randomY] = 1;
-				randomX+=borderCheck;
+			
+			if((ship.size + randomX) > 10) {
+				for(int i = 0; i < ship.size; i++) {
+					playerBoard.gameBoard[randomX][randomY] = 1;
+					randomX-=1;
+					
+				}
+				
+			}
+			else {
+				for(int i = 0; i < ship.size; i++) {
+					playerBoard.gameBoard[randomX][randomY] = 1;
+					randomX+=1;
+				}
+				
 			}
 		}
-		playerBoard.displayBoard();
+		
 	}
 	
 		
--- maintest.java
+++ maintest.java
@@ -2,10 +2,10 @@
 public class maintest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		BattleShipModel n = new BattleShipModel();
 		System.out.println();
-		n.placeRandomPosition(n.ship);
+		n.callRandom();
 		
 	}
 
