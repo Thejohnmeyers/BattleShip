da456590fb8a568319902e546799377093a68676
John Meyers <jmeyers5@gulls.salisbury.edu>
2023-10-03 21:19:26 -0400

    added two button grids

--- GUITest.java
+++ GUITest.java
@@ -1,9 +1,42 @@
-public class GUITest extends JFrame{
-    JButton[][] oppBoardView;
-    for(int row = 0; row < 10; row++){
-        for(int col = 0; col < 10; col++){
-            oppBoardView[row][col] = new JButton("");
-        }
-    }
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+class GUITest extends JFrame{
     
+    public GUITest(){
+        JButton[][] oppBoardView = new JButton[10][10];
+        JButton[][] playerBoardView = new JButton [10][10];
+        for(int row = 0; row < 10; row++){
+            for(int col = 0; col < 10; col++){
+                oppBoardView[row][col] = new JButton("");
+                playerBoardView[row][col] = new JButton("");
+            }
+        }
+        JFrame frame = new JFrame();
+	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    JPanel buttonPanel = new JPanel();
+        JPanel bottomPanel = new JPanel();
+	    JPanel containerPanel = new JPanel();
+       // buttonPanel.setLayout(new GridLayout(10,10));
+        for(int row = 0; row < 10; row++){
+            for(int col = 0; col < 10; col++){
+                buttonPanel.add(oppBoardView[row][col]);
+                bottomPanel.add(playerBoardView[row][col]);
+            }
+        }
+       
+        buttonPanel.setPreferredSize(new Dimension(400, 400));
+        bottomPanel.setPreferredSize(new Dimension(400, 400));
+	    frame.getContentPane().add(buttonPanel, BorderLayout.NORTH);
+        frame.getContentPane().add(bottomPanel, BorderLayout.SOUTH);
+	    //frame.getContentPane().add(containerPanel);
+	    frame.pack();
+	    frame.setVisible(true);
+
+    }
+
 }
--- maintest.java
+++ maintest.java
@@ -2,7 +2,7 @@
 public class maintest {
 
 	public static void main(String[] args) {
-		
+		GUITest t = new GUITest();
 		BattleShipModel n = new BattleShipModel();
 		System.out.println();
 		n.callRandom();
