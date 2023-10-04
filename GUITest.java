726f1f0a49a7ee4746b6a502621006cedcfa3d55
John Meyers <jmeyers5@gulls.salisbury.edu>
2023-10-03 20:24:50 -0400

    adding guitest

---
+++ GUITest.java
@@ -0,0 +1,9 @@
+public class GUITest extends JFrame{
+    JButton[][] oppBoardView;
+    for(int row = 0; row < 10; row++){
+        for(int col = 0; col < 10; col++){
+            oppBoardView[row][col] = new JButton("");
+        }
+    }
+    
+}
