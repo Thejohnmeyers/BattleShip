f7713141d08aba8a869b708043d7aa867483ab17
John Meyers <jmeyers5@gulls.salisbury.edu>
2023-10-04 21:35:21 -0400

    adding some images

--- GUITest.java
+++ GUITest.java
@@ -1,8 +1,11 @@
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.GridLayout;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 class GUITest extends JFrame{
@@ -19,8 +22,14 @@ class GUITest extends JFrame{
         JFrame frame = new JFrame();
 	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new GridLayout(10,10));
+        ImageIcon image1 = new ImageIcon("top-warship-icon-cartoon-military-ship-vector.jpg");
+        ImageIcon image2 = new ImageIcon("army-ship-icon-cartoon-military-navy-vector-44931653.jpg");
+        ImageIcon image3 = new ImageIcon("ship3.png");
+        ImageIcon image4 = new ImageIcon("250-ship-14-256-1.jpg");
         JPanel bottomPanel = new JPanel();
-	    JPanel containerPanel = new JPanel();
+        bottomPanel.setLayout(new GridLayout(10,10));
+	    //JPanel containerPanel = new JPanel();
        // buttonPanel.setLayout(new GridLayout(10,10));
         for(int row = 0; row < 10; row++){
             for(int col = 0; col < 10; col++){
@@ -29,9 +38,13 @@ class GUITest extends JFrame{
             }
         }
        
-        buttonPanel.setPreferredSize(new Dimension(400, 400));
-        bottomPanel.setPreferredSize(new Dimension(400, 400));
+        buttonPanel.setPreferredSize(new Dimension(200, 300));
+        bottomPanel.setPreferredSize(new Dimension(200, 300));
 	    frame.getContentPane().add(buttonPanel, BorderLayout.NORTH);
+        frame.add(new JLabel(image1), BorderLayout.WEST);
+        frame.add(new JLabel(image2));
+        frame.add(new JLabel(image3));
+        frame.add(new JLabel(image4), BorderLayout.EAST);
         frame.getContentPane().add(bottomPanel, BorderLayout.SOUTH);
 	    //frame.getContentPane().add(containerPanel);
 	    frame.pack();
